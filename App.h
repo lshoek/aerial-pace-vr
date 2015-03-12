02dea7dd6fa38cee0c15299c3e9aa35b25ac582a
DenBaas <basvanloon@live.nl>
2015-03-12 12:01:48 +0100

    added bullet, but nothing else

--- App.h
+++ App.h
@@ -5,6 +5,31 @@
 #include <VrLib\Kernel.h>
 #include <CaveLib\CaveLib.h>
 #include <GL\glew.h>
+
+
+
+#ifdef _DEBUG
+#pragma comment(lib, "Bullet3Common_vs2010_debug.lib")
+#pragma comment(lib, "Bullet3Collision_vs2010_debug.lib")
+#pragma comment(lib, "Bullet3Dynamics_vs2010_debug.lib")
+#pragma comment(lib, "BulletCollision_vs2010_debug.lib")
+#pragma comment(lib, "BulletDynamics_vs2010_debug.lib")
+#pragma comment(lib, "Bullet3Geometry_vs2010_debug.lib")
+#pragma comment(lib, "LinearMath_vs2010_debug.lib")
+#else
+#pragma comment(lib, "Bullet3Common_vs2010.lib")
+#pragma comment(lib, "Bullet3Collision_vs2010.lib")
+#pragma comment(lib, "Bullet3Dynamics_vs2010.lib")
+#pragma comment(lib, "BulletCollision_vs2010.lib")
+#pragma comment(lib, "BulletDynamics_vs2010.lib")
+#pragma comment(lib, "Bullet3Geometry_vs2010.lib")
+#pragma comment(lib, "LinearMath_vs2010.lib")
+#endif
+
+
+
+
+#include <../bullet3/src/btBulletDynamicsCommon.h>
 #include "WiiMoteWrapper.h"
 #include <ctime>
 #include "Camera.h"
