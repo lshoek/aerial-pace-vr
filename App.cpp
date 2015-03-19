8795eefd1930a50e4ad12f889aeed0fecb98898e
DenBaas <basvanloon@live.nl>
2015-03-19 12:44:09 +0100

    working on rotations

--- App.cpp
+++ App.cpp
@@ -11,40 +11,6 @@ App::App(WiiMoteWrapper * w){
 }
 App::~App(void){}
 
-/*nog niet weghalen
-=======
->>>>>>> 19d8423518e9017ce5359d44b96381e421faf365
-int App::updateCarSpeed(GLfloat timeFactor){
-	
-	if (!wiiMoteWrapper || !physics.world)
-		return -2;
-	if (wiiMoteWrapper->buttonHome)//pauze, zolang home is ingedrukt
-		return -1;
-	float newSpeed = 0;
-	if (wiiMoteWrapper->buttonOne)//speed goes down
-		newSpeed -= car.MAXFORCE * timeFactor;
-	if (wiiMoteWrapper->buttonTwo)//speed goes up
-		newSpeed += car.MAXFORCE * timeFactor;
-	newSpeed += car.MAXFORCE;
-	btVector3 deltaSpeed = btVector3(newSpeed, 0, 0);// .rotate(btVector3(0, 1, 0), btRadians(wiiMoteWrapper->degrees));
-	float r = (btRadians(wiiMoteWrapper->degrees) + car.carRadians);
-	car.carSpeed += newSpeed;
-	car.carRadians = r;
-	btQuaternion rotation = physics.realCar->getOrientation();
-	float rotationAngle = rotation.getAngle() + btRadians(30.0f);//wiiMoteWrapper->degrees
-	btQuaternion newRotation = btQuaternion(btVector3(0,1.0f,0), rotationAngle);
-	//newRotation *= rotation;
-	physics.realCar->getWorldTransform().setRotation(newRotation);
-	car.carRadians = newRotation.getAngle();
-	//physics.realCar->applyCentralForce(deltaSpeed);
-	physics.realCar->applyTorque(deltaSpeed);
-	physics.realCar->activate();
-	physics.world->stepSimulation(timeFactor);//en updaten	
-	btVector3 b = physics.realCar->getWorldTransform().getOrigin();
-	printf("auto %f,%f,%f :%f rad %f m/s\n", b.x(), b.y(), b.z(), physics.realCar->getOrientation().getAngle(), car.carSpeed);
-	return 1;//succes!
-}*/
-
 void App::init(void)
 {
 	//upArrow.init("UpArrow"); downArrow.init("DownArrow"); leftArrow.init("LeftArrow"); rightArrow.init("RightArrow");
@@ -88,9 +54,8 @@ void App::draw(const glm::mat4 &projectionMatrix, const glm::mat4 &modelViewMatr
 	camera->refresh();
 	btVector3 b = physics.realCar->getWorldTransform().getOrigin();
 	b = car.direction;
-	glTranslatef(b.x(), 0, b.z());
+	
 	//glTranslatef()
-	checkers_model->bbox.bounds;
 	float angle = physics.realCar->getOrientation().getAngle();
 	glRotatef(car.carRadians, 0, 1, 0);
 	checkers_model->draw();
@@ -110,6 +75,7 @@ void App::draw(const glm::mat4 &projectionMatrix, const glm::mat4 &modelViewMatr
 		glPopMatrix();
 	}
 	glScalef(100.0f, 100.0f, 100.0f);
+	glTranslatef(b.x(), 0, b.z());
 	DrawWireFrame();
 	//classicFont->drawText("HELLO WORLD!", 10.0f, 10.0f, 0.0f);
 }
